8549963778638f5aa1e8094916a8cc5e68e3c5d4
iluckyday <luckyday1990@mail.com>
2020-12-03 20:47:12 +0800

    test

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -ex
 
 VER=$(wget --no-check-certificate -qO- https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep -oE -m 1 'v[0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2}')
-URL=https://github.com/v2ray/v2ray-core/releases/download/$VER/v2ray-linux-64.zip
+URL=https://github.com/v2fly/v2ray-core/releases/download/$VER/v2ray-linux-64.zip
 
 wget --no-check-certificate -qO- $URL | unzip - -q -d /tmp v2ray v2ctl
 chmod +x /tmp/v2ctl /tmp/v2ray
