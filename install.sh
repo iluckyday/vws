6afd624b3f2e707ceadbe0effe85b35126d023ec
iluckyday <luckyday1990@mail.com>
2020-12-03 20:04:35 +0800

    test

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VER=$(wget --no-check-certificate -q -O- https://api.github.com/repos/v2ray/v2ray-core/releases/latest | awk -F'"' '/tag_name/ {print $4}')
+VER=$(check-certificate -q -O- https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep -oE -m 1 'v[0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2}')
 URL=https://github.com/v2ray/v2ray-core/releases/download/$VER/v2ray-linux-64.zip
 
 wget --no-check-certificate -q -O- $URL | unzip - -q -d /tmp v2ray v2ctl
